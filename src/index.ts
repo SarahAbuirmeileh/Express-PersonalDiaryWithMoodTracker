9c10ead78cd0da2a9c9b70ff6cc15f97e0267f08
Nour AlHuda Natsheh <nouralhudanatsheh@Nouralhudas-MacBook-Air.local>
2025-07-22 21:47:20 +0300

    add quotes route to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import { connectDB } from './db/connection.js';
-import userRouter from './routes/user.router.js';
-import tagRouter from './routes/tag.router.js';
-import cookieParser from 'cookie-parser';
-import { authenticate } from './middlewares/auth/authenticate.js';
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./db/connection.js";
+import userRouter from "./routes/user.router.js";
+import tagRouter from "./routes/tag.router.js";
+import quoteRouter from "./routes/quote.router.js";
+import cookieParser from "cookie-parser";
+import { authenticate } from "./middlewares/auth/authenticate.js";
 
 const app = express();
 const port: number = 3000;
@@ -13,19 +14,20 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/users', userRouter);
-app.use('/tags', authenticate, tagRouter);
+app.use("/users", userRouter);
+app.use("/tags", authenticate, tagRouter);
+app.use("/quotes", quoteRouter);
 
-app.get('/', (req: express.Request, res: express.Response) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send("Hello world!");
 });
 
 // Catch all routes
-app.all('/{*notFound}', (req: express.Request, res: express.Response) => {
+app.all("/{*notFound}", (req: express.Request, res: express.Response) => {
   res.status(404).send("Not found!");
 });
 
 app.listen(port, () => {
-  console.log(`Hello express app listening on port ${port}`)
+  console.log(`Hello express app listening on port ${port}`);
   connectDB();
 });
